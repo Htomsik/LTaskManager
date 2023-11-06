fe041859960643acec7aeaf7643c5ccc15f64c5e
Htomsik <JessJake@proton.me>
2023-11-07 01:17:24 +0300

    Fetch: Add Navigation infrastructure

---
+++ LiteTaskManager/Front/Client/Views/ProcessesView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Client.Views;
+
+public partial class ProcessesView : UserControl
+{
+    public ProcessesView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
