749ef9606ae142eff9296e1b4e2859ede6120f07
Konstantin <83224669+Htomsik@users.noreply.github.com>
2023-11-17 23:27:17 +0300

    Merge: Add sample validation
    * Fetch: Add Enum to collection converter
    
    * Fetch: Add change language settings
    
    * Fix: fix update process
    
    * Fetch: Add validation sample in AppSettings

--- LiteTaskManager/Front/Client/Models/AppSettings.cs
+++ LiteTaskManager/Front/Client/Models/AppSettings.cs
@@ -1,35 +1,54 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
 
 namespace Client.Models;
 
 /// <summary>
 ///     Модель конфигцрации приложения
 /// </summary>
-internal sealed class AppSettings : ReactiveObject
+internal sealed class AppSettings : ReactiveValidationObject
 {
     /// <summary>
     ///     Делей между обновлениями процессов
-    /// TODO: когда будем внедрять валидацию, сделать проверку на то чтобы нельзя было обновлять меньше 2-3 секунд
     /// </summary>
-    public double ProcessUpdateTimeOut 
+    public double ProcessUpdateTimeOut
     {
         get => _processUpdateTimeOut;
         set
         {
-            if (value < 3)
-                value = 3;
+            if (value < 0)
+            {
+                value = 0;
+            }
             
-            _processUpdateTimeOut = value;
-            this.RaisePropertyChanged(nameof(ProcessUpdateTimeOut));
+            this.RaiseAndSetIfChanged(ref _processUpdateTimeOut, value);
         }
     }
-    
-    private double _processUpdateTimeOut = 5;
-    
+
+    private double _processUpdateTimeOut;
+
     /// <summary>
     ///     Текущая выбранная локализация
     /// </summary>
     [Reactive]
     public AppCulture Culture { get; set; }
+
+
+    public AppSettings()
+    {
+        SetValidation();
+    }
+
+    /// <summary>
+    ///     Устанавливает валидацию для текущей модели
+    /// </summary>
+    private void SetValidation()
+    {
+       this.ValidationRule(
+            appSettings => appSettings.ProcessUpdateTimeOut, 
+            value => value >= 4,
+            "TimeOut must be more than 3 seconds");
+    }
 }
