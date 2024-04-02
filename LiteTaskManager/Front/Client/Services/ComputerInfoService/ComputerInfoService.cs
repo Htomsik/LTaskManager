f288f8ad8418b7e257dd381310aa4011fd84c443
Htomsik <JessJake@proton.me>
2024-04-02 22:30:42 +0300

    Fetch: Add WMI service

---
+++ LiteTaskManager/Front/Client/Services/ComputerInfoService/ComputerInfoService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Client.Models;
+using Client.Services.WMIService;
+
+
+namespace Client.Services.ComputerInfoService;
+
+public sealed class ComputerInfoService : IComputerInfoService
+{
+   private readonly IWmiService _wmiService;
+
+   #region Properties
+
+   public double TotalPhysicalMemoryBytes { get; set; }
+
+   #endregion
+
+   #region Constructor
+
+   public ComputerInfoService(IWmiService wmiService)
+   {
+      _wmiService = wmiService;
+
+      if (OperatingSystem.IsWindows())
+      {
+         InitForWindows();
+      }
+      else
+      {
+         InitForUnix();
+      }
+   }
+
+   #endregion
+
+   #region Methods
+
+   private void InitForWindows()
+   {
+      string data;
+
+      data = _wmiService.GetHardwareInfo(WmiElement.TotalPhysicalRAM.Item1, WmiElement.TotalPhysicalRAM.Item2);
+
+      if (double.TryParse(data, out var parsed))
+      {
+         TotalPhysicalMemoryBytes = parsed;
+      }
+   }
+
+   private void InitForUnix()
+   {
+      // TODO Подумать нужно ли вообще под Linux писать 
+   }
+
+   #endregion
+   
+}
---
+++ LiteTaskManager/Front/Client/Services/WMIService/WMIService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using Client.Infrastructure.Logging;
+using ReactiveUI;
+using Splat;
+
+namespace Client.Services.WMIService;
+
+public class WmiService : ReactiveObject, IWmiService
+{
+    public string GetHardwareInfo(string win32Class, string classItemField)
+    {
+        var result = new List<string>();
+
+        var searcher = new ManagementObjectSearcher("SELECT * FROM " + win32Class);
+
+        try 
+        {
+            foreach (var obj in searcher.Get()) 
+            {
+                result.Add(obj[classItemField].ToString().Trim());
+            }
+        }
+        catch (Exception e) 
+        {
+            this.Log().StructLogError($"Can't get {win32Class}:{classItemField}",e.Message);
+        }
+
+        return string.Join(", ", result);
+    }
+}
