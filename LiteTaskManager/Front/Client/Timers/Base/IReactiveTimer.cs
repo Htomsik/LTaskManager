66bcd8e3bc5260ea9e70ea03ba208477f1a7364f
Htomsik <JessJake@proton.me>
2024-04-04 19:46:50 +0300

    Fetch: Create reactive timer

---
+++ LiteTaskManager/Front/Client/Timers/Base/IReactiveTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Client.Timers.Base;
+
+/// <summary>
+///     Таймер 
+/// </summary>
+public interface IReactiveTimer
+{
+    /// <summary>
+    ///     Делей между обновлениями
+    /// </summary>
+    public int UpdateDelaySeconds { get; set; }
+    
+    /// <summary>
+    ///     Пересоздавать ли таймер заново при истечении времени
+    /// </summary>
+    public bool ReCreate { get; set; }
+    
+    /// <summary>
+    ///     Уведомлятор о том сколько времени осталось
+    /// </summary>
+    public event Action<long>? TimerChangedNotifier;
+    
+    /// <summary>
+    ///     Стартовать таймера 
+    /// </summary>
+    public void Start();
+    
+    /// <summary>
+    ///     Остановка таймера
+    /// </summary>
+    public void Stop();
+}
