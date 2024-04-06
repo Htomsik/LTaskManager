379466ebec79933a71e18ab19b2c655299c5472e
Htomsik <JessJake@proton.me>
2024-04-06 15:32:48 +0300

    Fetch: Add color indications of ram/cpu usage

---
+++ LiteTaskManager/Front/Client/Extensions/ResourcesExtension.cs
@@ -0,0 +1,52 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace Client.Extensions;
+
+/// <summary>
+///     Расширение для получения ресурсов из приложения
+/// </summary>
+public static class ResourcesExtension
+{
+    #region Brushes
+    
+    /// <summary>
+    ///     Получает кисть из ресурсов и записывает сразу в бекграуд
+    /// </summary>
+    private static IBrush? GetBrush(string name, ref IBrush? value)
+    {
+        if (value is not null)
+        {
+            return value;
+        }
+
+        var found =  Application.Current!.TryGetResource(name, Application.Current.ActualThemeVariant, out var brush);
+
+        if (found)
+        {
+            value = (IBrush?)brush;
+        }
+            
+        return value;
+    }
+    
+    public static IBrush? Percent75Brush => GetBrush("Percent75", ref _percent75Brush);
+    
+    private static IBrush? _percent75Brush;
+    
+    
+    public static IBrush? Percent50Brush => GetBrush("Percent50", ref _percent50Brush);
+    
+    private static IBrush? _percent50Brush;
+    
+    
+    public static IBrush? Percent25Brush => GetBrush("Percent25", ref _percent25Brush);
+    
+    private static IBrush? _percent25Brush;
+    
+    public static IBrush? Percent0Brush => GetBrush("Percent0", ref _percent0Brush);
+    
+    private static IBrush? _percent0Brush;
+    
+    #endregion
+}
--- LiteTaskManager/Front/Client/Infrastructure/Converters/Converters.cs
+++ LiteTaskManager/Front/Client/Infrastructure/Converters/Converters.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Globalization;
 using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Client.Extensions;
 
 namespace Client.Infrastructure.Converters;
 
@@ -12,6 +14,40 @@ public class ObjectToStringConverter : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is null ? string.Empty : value.ToString();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+/// <summary>
+///     Конвертер процентов в цвета
+/// </summary>
+public class DoublePercentToColor : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        IBrush? brush = null;
+        
+        if (value is not double percent)
+        {
+            return brush;
+        }
+
+        percent*= 100;
+
+        brush = percent switch
+        {
+            >= 75 => ResourcesExtension.Percent75Brush,
+            >= 50 => ResourcesExtension.Percent50Brush,
+            >= 25 => ResourcesExtension.Percent25Brush,
+            > 0 => ResourcesExtension.Percent0Brush,
+            _ => brush
+        };
+
+        return brush;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
